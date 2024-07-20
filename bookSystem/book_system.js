66cb8cb2ff62d72f4a40ac3dbcbfac5857dfbeb2
steeve-496 <steevebinubaby496@gmail.com>
2024-07-20 13:14:36 -0400

    updated Book Manager added button button for book

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -15,7 +15,7 @@ function addBook(){
         const added= `<p>Book added Successfully</p>`;
         document.getElementById('action').innerHTML = added;
         showbooks();
-        // clearInputs();
+        clearInputs();
     }else{
         alert("Please fill in all fields correctly.");
     }
@@ -26,17 +26,30 @@ function showbooks(){
         <p><strong>Book Name\t\t\t:</strong> ${book.name}</p>
         <p><strong>Author Name\t\t:</strong> ${book.authorName}</p>
         <p><strong>Book Description\t: </strong>${book.bookDescription}</p>
-        <p><strong>No. of Pages\t\t: </strong>${book.pagesNumber} pages</p>`
-    );
+        <p><strong>No. of Pages\t\t: </strong>${book.pagesNumber} pages</p>
+        <p><button onclick="deletebook(${index})">Delete Book</button></p>`
+        );
     document.getElementById('books').innerHTML = booksDiv.join('');
 }
 
-// function clearInputs(){
-//     document.getElementById('bookName').value = '';
-//     document.getElementById('authorName').value= '';    
-//     document.getElementById('bookDescription').value = '';
-//     document.getElementById('pagesNumber').value = '';
-// }
+function deletebook(index){
+    if (index >= 0 && index < books.length) {
+        books.splice(index, 1); // Remove 1 element at position 'index' from the 'books' array
+        showbooks(); // Update the displayed list of books
+        document.getElementById('deleted').innerHTML = `<p>Book ${index+1} deleted Successfully</p>`;
+    } else {
+        alert("Invalid index provided.");
+    }
+}
+
+
+
+function clearInputs(){
+    document.getElementById('bookName').value = '';
+    document.getElementById('authorName').value= '';    
+    document.getElementById('bookDescription').value = '';
+    document.getElementById('pagesNumber').value = '';
+}
 
 function clearInput(){
     document.getElementById('bookName').value = '';
