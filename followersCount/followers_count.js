8f7d86c9f68945d5eab6844efea7c9cdd1edb670
steeve-496 <steevebinubaby496@gmail.com>
2024-07-20 02:25:33 -0400

    Added followers_count

---
+++ followersCount/followers_count.js
@@ -0,0 +1,17 @@
+let count=0;
+function increaseCount(){
+    count++;
+    displayCount();
+    checkCountValue();
+}
+
+function displayCount(){
+    document.getElementById("countDisplay").innerHTML=count;
+}
+function checkCountValue(){
+    if(count === 10){
+        alert("Your instagram post gained 10 followers! Congratulations!");
+    }else if (count===20){
+        alert("Your instagram post gained 20 followers! Congratulations!Keep it up") 
+    }
+}
