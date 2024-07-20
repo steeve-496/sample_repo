55e0551465c809b5e26ee71925c4fee74048d966
steeve-496 <steevebinubaby496@gmail.com>
2024-07-20 08:49:49 -0400

    Resturant Menu Using Array is added

---
+++ restaurantMenu/restaurant_menu.js
@@ -0,0 +1,30 @@
+const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
+const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
+const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+
+const breakfastMenuItemsHTML= breakfastMenu.map((item,index) => `<p>Item ${index+1} : ${item}</p>`).join('');
+document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+
+//using map
+// const mainCourseMenuItemsHTML = mainCourseMenu.map((item,index)=> `<p>Item ${index+1} : ${item}</p>`).join('');
+// document.getElementById('maincourseMenuItems').innerHTML = mainCourseMenuItemsHTML;
+
+//using forEach
+let mainCourseItem='';
+mainCourseMenu.forEach((item,index)=> {
+    mainCourseItem += `<p>Item ${index+1} : ${item}</p>`;
+});
+document.getElementById('maincourseMenuItems').innerHTML =  mainCourseItem;
+
+
+
+//using map
+// const dessertMenuItemsHTML=dessertMenu.map((item,index)=> `<p>Item ${index+1} : ${item}</p>`).join('');
+// document.getElementById('dessertMenuItems').innerHTML= dessertMenuItemsHTML;
+
+//Using For loop
+let dessertItem='';
+for(let i = 0;i < dessertMenu.length; i++) {
+    dessertItem += `<p>Item ${i+1} : ${dessertMenu[i]}</p>`;
+}
+document.getElementById('dessetMenuItems').innerHTML = dessertItem;
